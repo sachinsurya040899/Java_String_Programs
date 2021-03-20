0dfa5140e525eb4221163b9e70a5c45907fac1b2
Sachinsurya K <58526675+sachinsurya040899@users.noreply.github.com>
2021-03-20 07:57:17 +0530

    Add files via upload

--- String_10.java
+++ String_10.java
@@ -1,72 +1,55 @@
 /*
-
-Indian PAN card issuing authority have found some fake PAN cards. 
-They have hired you so that you can validate PAN card for them. 
-Your task is to develop a suitable algorithm which could check if pan is valid or not
-
-1)Pan must have uppercase letters only.
-
-2)It must be of 10 character only
-
-3)From index 1 to 5 all must be letters(A-Z),last index must be letter
-
-4)Rest all must be integer Starting from 1
+You are given a string ‘s’. Your task is to print the characters which are not repeated with a single space in between the characters.
 
 Input Description:
-==================
-You are given a input string which indicates the PAN number
+===================
+You are given a string ‘s’.
 
 Output Description:
 ===================
-Print 'pan' if it is valid PAN number, else print 'not pan'
+Print the characters present once and -1 if there is no character which satisfy above condition
 
 Sample Input :
 ==============
-HXTPS2142R
+dabbc
 
 Sample Output :
 ===============
-pan
+d a c
+
+Input:
+======
+ddddddddabbc
+
+Expected Output:
+================
+a c
 
 */
 import java.util.Scanner;
-class String_10 
-{
-	public static void main(String[] args) 
-	{
+public class String_1 {
+	public static void main(String[] args) {
 		Scanner sa=new Scanner(System.in);
-		String str=sa.next();
-		String s[]=str.split("");
-		int ucheck=6;
-		int len=s.length;
-		int lindex=len-1;
-		int index_check=6;
-		int number_check=4;
-		if(len==10){
-			for(int i=0;i<s.length;i++){
-				if(s[i].charAt(0)>=65 && s[i].charAt(0)<=90){
-					ucheck--;
+		String str=sa.nextLine();
+		String ans="";
+		for(int i=0;i<str.length();i++) {
+			for(int j=i+1;j<str.length();j++) {
+				if(str.charAt(i)==str.charAt(j)) {
+					i++;
+					break;
 				}
-				if(i<=4 || i==lindex){
-					if(s[i].charAt(0)>=65 && s[i].charAt(0)<=90){
-						index_check--;
-					}
-				}
-				if(i>4 && i<lindex){
-					if(s[i].charAt(0)>=48 && s[i].charAt(0)<=57){
-						number_check--;
-					}
+				else {
+					ans=ans+str.charAt(i)+" ";
+					break;
 				}
 			}
+			if(i==str.length()-1 && (str.charAt(i-1)!=str.charAt(i))){
+				ans=ans+str.charAt(i);
+			}
 		}
-		else{
-			System.out.print("not pan");
-		}
-		if(index_check==0 && number_check==0 && ucheck==0){
-			System.out.print("pan");
-		}
-		else if(len==10){
-			System.out.print("not pan");
-		}
+		if(!ans.equals(""))
+			System.out.println(ans);
+		else
+			System.out.println("-1");
 	}
 }
